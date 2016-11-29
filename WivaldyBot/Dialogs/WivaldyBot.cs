d500bb7374190f5e3092bdaeb56b59a7daef5c03
Ellerbach <laurelle@microsoft.com>
2016-11-29 15:03:42 +0100

    Initial commit

---
+++ WivaldyBot/Dialogs/WivaldyBot.cs
@@ -0,0 +1,157 @@
+﻿using Microsoft.Bot.Builder.Dialogs;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Threading.Tasks;
+using Microsoft.Bot.Connector;
+using WivaldyBot.Helpers;
+using WivaldyBot.Models;
+using static WivaldyBot.Models.Wivaldy;
+using System.Globalization;
+
+namespace WivaldyBot.Dialogs
+{
+    [Serializable]
+    public class WivaldyDialog : IDialog<object>
+    {
+
+        private const string DialogConsumptionInstant = "Instant";
+        private const string DialogConsumptionHour = "Last hour";
+        private const string DialogConsumptionDay = "Today";
+        private const string DialogConsumptionLastDay = "Last day";
+        private const string WivaldiConnectionString = "wivaldystring";
+
+        private Wivaldy myWivaldy = new Wivaldy();
+
+        private ResumptionCookie resumptionCookie;
+        public async Task StartAsync(IDialogContext context)
+        {
+            context.Wait(this.MessageReceivedAsync);
+        }
+
+        public virtual async Task MessageReceivedAsync(IDialogContext context, IAwaitable<IMessageActivity> result)
+        {
+            var message = await result;
+            if (this.resumptionCookie == null)
+            {
+                this.resumptionCookie = new ResumptionCookie(message);
+            }
+
+            string wiwaldyconnection;
+
+            if (!context.UserData.TryGetValue(WivaldiConnectionString, out wiwaldyconnection))
+            {
+                PromptDialog.Text(context, this.ResumeAfterPrompt, "Before get started, can you please give me your private key?");
+                return;
+            } else
+            {
+                myWivaldy.Connection = wiwaldyconnection;
+            }
+
+            await this.WelcomeMessageAsync(context);
+        }
+
+        private async Task WelcomeMessageAsync(IDialogContext context)
+        {
+            var reply = context.MakeMessage();
+
+            var options = new[]
+            {
+                DialogConsumptionInstant,
+                DialogConsumptionHour,
+                DialogConsumptionDay,
+                DialogConsumptionLastDay
+            };
+            reply.AddHeroCard(
+                "Electric consumption",
+                "Please select the cunsomption you want to see",
+                options,
+                new[] { "http://storage.googleapis.com/instapage-user-media/2befcf7f/6297728-0-wivaldy-all.png" });
+
+            await context.PostAsync(reply);
+
+            context.Wait(this.OnOptionSelected);
+        }
+
+        private async Task OnOptionSelected(IDialogContext context, IAwaitable<IMessageActivity> result)
+        {
+            var message = await result;
+            var reply = context.MakeMessage();
+            string strresp = "Your total consumption for ";
+            Electricity res = null;
+
+            if (message.Text == DialogConsumptionInstant)
+            {
+                res = await myWivaldy.GetLastMeasures();
+                strresp += "now is ";
+            }
+            else if (message.Text == DialogConsumptionHour)
+            {
+                res = await myWivaldy.GetMeasures(DateTimeOffset.Now.AddHours(-1), DateTimeOffset.Now);
+                strresp += "the last hour is ";
+            }
+            else if (message.Text == DialogConsumptionDay)
+            {
+                DateTimeOffset today = new DateTimeOffset(DateTimeOffset.Now.Year, DateTimeOffset.Now.Month, DateTimeOffset.Now.Day, 0, 0, 0, DateTimeOffset.Now.Offset);
+                res = await myWivaldy.GetMeasures(today, today.AddDays(1));
+                strresp += "today is ";
+            }
+            else if (message.Text == DialogConsumptionLastDay)
+            {
+                DateTimeOffset yesterday = new DateTimeOffset(DateTimeOffset.Now.Year, DateTimeOffset.Now.Month, DateTimeOffset.Now.Day, 0, 0, 0, DateTimeOffset.Now.Offset);
+                yesterday = yesterday.AddDays(-1);
+                res = await myWivaldy.GetMeasures(yesterday, yesterday.AddDays(1));
+                strresp += "yesterday is ";
+            }
+
+            if (res != null)
+            {
+                if (res.Consumptions.Length > 1)
+                {
+                    long epochmin = res.Consumptions[0].epoch;
+                    long epochmax = epochmin;
+                    double wattshour = 0;
+                    foreach (var elec in res.Consumptions)
+                    {
+                        wattshour += elec.watts*(elec.epoch-epochmin);
+                        epochmin = elec.epoch;
+                    }
+                    wattshour = wattshour / 3600;
+                    strresp += $"{wattshour.ToString("N0", CultureInfo.CurrentUICulture)} kWh.";
+                }
+                else
+                {
+                    strresp += $"{res.Consumptions[0].watts.ToString("N0", CultureInfo.CurrentUICulture)} watts instanatannés.";
+                }
+            }
+            else
+            {
+                strresp = "Ups, something went wrong, can't get the consumption.";
+            }
+
+            reply.Text = strresp;
+            reply.TextFormat = "markdown";
+            await context.PostAsync(reply);
+            context.Wait(MessageReceivedAsync);
+        }
+
+        private async Task ResumeAfterPrompt(IDialogContext context, IAwaitable<string> result)
+        {
+            try
+            {
+                var wivaldyconnection = await result;
+
+                await context.PostAsync($"Thanks, now I have your jey and can get your electricity details!");
+
+                context.UserData.SetValue(WivaldiConnectionString, wivaldyconnection);
+            }
+            catch (TooManyAttemptsException)
+            {
+            }
+
+            context.Wait(this.MessageReceivedAsync);
+        }
+
+    }
+}
---
+++ WivaldyBot/Models/Wivaldy.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Diagnostics;
+using System.Threading.Tasks;
+
+namespace WivaldyBot.Models
+{
+    [Serializable]
+    public class Wivaldy
+    {
+        public string Connection { get; set; }
+
+        public Wivaldy()
+        {
+
+        }
+
+        public Wivaldy(string connection)
+        {
+            Connection = connection;
+        }
+
+        public async Task<Electricity> GetMeasures(DateTimeOffset start, DateTimeOffset stop)
+        {
+            //API is http://app.wivaldy.com/api/v1/json/privatekey/2016-11-01-08:00/2016-11-01-08:10
+            //API is http://app.wivaldy.com/api/v1/json/ {PRIVATE_KEY} /last
+            try
+            {
+                HttpClient cli = new HttpClient();
+                string urlrequest = $"http://app.wivaldy.com/api/v1/json/{Connection}/";
+                //if both are null, then only last measure
+                if ((start == DateTimeOffset.MinValue) && (stop == DateTimeOffset.MinValue))
+                    urlrequest += "last";
+                //if only stop is nullm than a full day
+                else if (stop == DateTimeOffset.MinValue)
+                    urlrequest += start.ToString("yyyy-MM-dd") + "/" + start.AddDays(1).ToString("yyyy-MM-dd");
+                else
+                {
+                    //TEMP FIX TO REMOVE WHILE NO DATA
+                    start = start.AddDays(-8);
+                    stop = stop.AddDays(-8);
+                    // END REMOVE
+                    if (DateTimeOffset.Compare(start, stop) > 1)
+                    { stop = start.AddDays(1); }
+                    urlrequest += start.ToString("yyyy-MM-dd-hh:mm") + "/" + stop.ToString("yyyy-MM-dd-hh:mm");
+                }
+                var str = await cli.GetStringAsync(new Uri(urlrequest));
+                Electricity elec = new Electricity();
+                if (str.Contains("["))
+                {
+                    var measuregroup = JsonConvert.DeserializeObject<List<Consumption>>(str);                  
+                    elec.Consumptions = measuregroup.ToArray();                    
+                }
+                else
+                {
+                    var measure = JsonConvert.DeserializeObject<Consumption>(str);
+                    elec.Consumptions = new Consumption[1];
+                    elec.Consumptions[0] = measure;
+                }
+                return elec;
+            }
+            catch (Exception ex)
+            {
+                Debug.WriteLine($"exception: {ex.Message}");
+            }
+            return null;
+        }
+
+        public async Task<Electricity> GetLastMeasures()
+        {
+            return await GetMeasures(DateTimeOffset.MinValue, DateTimeOffset.MinValue);
+        }
+
+        public async Task<Electricity> GetDayMeasures(DateTimeOffset start)
+        {
+            return await GetMeasures(start, DateTimeOffset.MinValue);
+        }
+
+        public class Electricity
+        {
+            public Consumption[] Consumptions { get; set; }
+        }
+
+        public class Consumption
+        {
+            public int epoch { get; set; }
+            public int watts { get; set; }
+        }
+
+
+        public class Rootobject
+        {
+            public Class1[] Property1 { get; set; }
+        }
+
+        public class Class1
+        {
+            public int epoch { get; set; }
+            public int watts { get; set; }
+        }
+
+    }
+}
